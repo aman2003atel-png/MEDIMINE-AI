5b2305020e0c8ab8c86d89419934f6a5f930abd5
Amanpa129 <aman260atel@gmail.com>
2026-01-04 14:23:56 +0530

    requiremts added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medimine_chatbot_ai",
+    version="0.1.0",
+    author="Aman Patel",
+    author_email="aman2003atel@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
